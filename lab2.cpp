169db3cb385b110401bcaaefd983775f1e8ed49f
Dahlberg <dahlbergs@wit.edu>
2016-09-15 10:45:39 -0400

    Add prompt for given time.

--- lab2.cpp
+++ lab2.cpp
@@ -27,14 +27,40 @@ int main() {
     }
 
     if (selection == "1") {
-        cout << "Please enter a day: " << endl;
+        cout << "Please enter a day from 1 to 7: " << endl;
+
+        string day_selection = "";
+        while (!getline(cin, day_selection) ||
+               (selection != "1" &&
+                selection != "2" &&
+                selection != "3" &&
+                selection != "4" &&
+                selection != "5" &&
+                selection != "6" &&
+                selection != "7")) {
+            cout << "Invalid selection, use number corresponding to day" << endl;
+            cout << "Selection: ";
+        }
+        int day = stoi(day_selection);
+
+        cout << "Please enter hour time from 0 to 23: ";
+
+        string time_selection = "";
+
+        while (!getline(cin, time_selection) ||
+               stoi(time_selection < 0) ||
+               stoi(time_selection > 23)) {
+            cout << "Invalid selection, use time from 0 to 23:";
+        }
+
+        int time = stoi(time_selection);
+
+        cout << "Action is: " << getAction(day, time)
     }
 }
 
 string getAction(int day, int time) {
-
     srand(day + time);
 
     return actions[rand() % actions.size()];
-
 }
